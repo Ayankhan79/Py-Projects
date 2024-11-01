8936f99ae1920fc6038fa93b4ea74301147249b1
Ayan Khan <116588018+Ayankhan79@users.noreply.github.com>
2024-11-02 02:26:52 +0530

    Update Screenshot_App_GUI.py

--- Screenshot_App_GUI.py
+++ Screenshot_App_GUI.py
@@ -1,17 +1,41 @@
-import time
+#Install Python neccesary libraries for this Program/Application
+
+#pip install pyautogui
+
+#pip install pillow
+
+#pip install tkinter
+
+#pyautogui for taking screenshots 
+#pillow library in Python is used for image processing tasks
+#tkinter for building the GUI(Graphical User Interface)
+
+
+#->we will want to import a library, which is a built-in module in py
+
 import pyautogui
+import time
 
 #######################################################################################################
 '''
 
 def screenshot():
-    time.sleep(5);                          #5Secs#whenever we call this function , it should wait for sometime and then take a click;
-    img = pyautogui.screenshot('test.png');    #itll take ss and save it as test.png 
-    img.show();                              #after clicking itll show the pic once
-    
-screenshot();                                      #to run the function
+#defining The Function named 'screenshot'
 
-#but here problem is its again and again updating the test.png file , losing the data of previous pic (overwritten)
+    time.sleep(5)                          
+    #5Secs#whenever we call this function , it should wait for sometime and then take a click;
+    
+    img = pyautogui.screenshot('test.png');    
+    #itll take ss and save it as test.png 
+    
+    img.show()                              
+    #after clicking itll show the pic once
+    
+    
+screenshot()      
+#to run the function
+
+#But, Here the problem is, it's again and again updating the 'test.png file' , losing the data of previous pic (overwritten)
 
 '''
 
@@ -21,26 +45,41 @@ screenshot();                                      #to run the function
 #updated code:
 
 def screenshot():
-    name = int(round(time.time() * 1000)) #converting random numbers into integers by rounding it and saving it as a variable 'name'
-    name = 'C:/Users/ayxnk/Desktop/python/screenshots data/{}.png'.format(name)  #itll save the pic with the random number generated.png 
-    #->itll save the pic in a particular folder, as the path given
-    time.sleep(5); #5Secs#whenever we call this function , it should wait for sometime and then take a click;
-    img = pyautogui.screenshot(name); #itll take ss and save it as test.png 
-    img.show();  #after clicking itll show the pic once
+defining the Function named 'screenshot'
+
+    name = int(round(time.time() * 1000)) 
+    #converting random numbers into integers by rounding it and saving it as a variable 'name'
     
-screenshot(); #to run the function
+    name = 'C:/Users/ayxnk/Desktop/python/screenshots data/{}.png'.format(name) 
+    #itll save the pic with the random number generated.png 
+    #->itll save the pic in a particular folder, as the path given
+    
+    time.sleep(5); 
+    #5Secs#whenever we call this function , it should wait for sometime and then take a click;
+    
+    img = pyautogui.screenshot(name); 
+    #itll take ss and save it as test.png 
+    
+    img.show();  
+    #after clicking itll show the pic once
+    
+    
+screenshot(); 
+#to run the function
 
 '''
 
 ###################################################################################################
 
 '''
-->now it is creating seperate pics for every pics or clicks taken
+->now, it is creating seperate pics for every pics or clicks taken
 
-->now if we want our pic/clicks to be saved in one folder then,
+->now if we want our pic/clicks to be saved in one single folder then,
 create folder, copy its path and paste that...
-->name = '......................{}.png'.format(name)
-path will be in "\" slashes, turn them into "/" ...
+
+      name = '......................{}.png'.format(name)
+
+path will be in "\" slashes, turn them into "/"
 
 '''
 
@@ -53,20 +92,31 @@ path will be in "\" slashes, turn them into "/" ...
 import tkinter as tk #we dont have to write the fullform everytime i.e tkinter, instead we can use tk
 
 def screenshot():
-    name = int(round(time.time() * 1000)) #converting random numbers into integers by rounding it and saving it as a variable 'name'
-    name = 'C:/Users/ayxnk/Desktop/python/screenshots data/{}.png'.format(name)  #itll save the pic with the random number generated.png 
+    
+    name = int(round(time.time() * 1000)) 
+    #converting random numbers into integers by rounding it and saving it as a variable 'name'
+    
+    name = 'C:/Users/ayxnk/Desktop/python/screenshots data/{}.png'.format(name)  
+    #itll save the pic with the random number generated.png 
     #->itll save the pic in a particular folder, as the path given
+    
     '''
     time.sleep(5); #5Secs#whenever we call this function , it should wait for sometime and then take a click;
     -> as we have button , we do not need timer, but its all your wish to keep it or not.
     '''
-    img = pyautogui.screenshot(name); #itll take ss and save it as test.png 
-    img.show();  #after clicking itll show the pic once
     
-    '''
-screenshot(); #to run the function
-#we have removed function call from here
-    '''
+    img = pyautogui.screenshot(name)
+    #itll take ss and save it as test.png 
+    
+    img.show()
+    #after clicking itll show the pic once
+    
+    
+'''
+screenshot();
+#to run the function
+#->we have removed function call from here
+'''
 
 root = tk.Tk()      
 #root is your main application window/This creates the main window (or root window) of your application.
@@ -93,6 +143,7 @@ button = tk.Button(
 )
 #The first button is created inside the frame, labeled "Take Screenshot". 
 #The command=screenshot tells tkinter to execute the screenshot function when this button is clicked.
+
 
 button.pack(side=tk.LEFT)      
 ##The button is placed on the left side of the frame using pack(side=tk.LEFT).
